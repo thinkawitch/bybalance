8ae5e7ee53411c2078fa8ddbd1a07f16b2ccca68
andrew <andrew12.hk@gmail.com>
2012-08-05 19:25:44 +0300

    select account type

---
+++ ByBalance/Classes/2.Account/BBAccountTypeCell.h
@@ -0,0 +1,13 @@
+//
+//  BBAccountTypeCell.h
+//  ByBalance
+//
+//  Created by Andrew Sinkevitch on 05/08/2012.
+//  Copyright (c) 2012 sinkevitch.name. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BBAccountTypeCell : NSObject
+
+@end
---
+++ ByBalance/Classes/2.Account/BBAccountTypeCell.m
@@ -0,0 +1,13 @@
+//
+//  BBAccountTypeCell.m
+//  ByBalance
+//
+//  Created by Andrew Sinkevitch on 05/08/2012.
+//  Copyright (c) 2012 sinkevitch.name. All rights reserved.
+//
+
+#import "BBAccountTypeCell.h"
+
+@implementation BBAccountTypeCell
+
+@end
