c4b9f656472f34305cc6f379f57269aa24febaa2
thinkawitch <andrew12.hk@gmail.com>
2013-02-18 21:22:32 +0300

    исправил МТС

--- ByBalance/Classes/App/Balance/Loader/BBLoaderMts.m
+++ ByBalance/Classes/App/Balance/Loader/BBLoaderMts.m
@@ -25,7 +25,8 @@
     //                                               forKeys:[NSArray arrayWithObjects:kDictKeyAccount, kDictKeyBaseItem, nil]];
     
     [request addRequestHeader:@"Referer" value:loginUrl];
-    [request setPostValue:@"/wEPDwUKMTU5Mzk3MTA0NA9kFgJmD2QWAgICDxYCHgVjbGFzcwUFbG9naW4WAgICD2QWBgIBDw8WAh4JTWF4TGVuZ3RoAglkZAIDDw8WAh4DS0VZBSJjdGwwMF9NYWluQ29udGVudF9jYXB0Y2hhMzA2MjI5NzAwZGQCBQ8PFgYeBFRleHRlHghDc3NDbGFzcwUGc3VibWl0HgRfIVNCAgJkZGRq1lFdf8Isy5ch/s7SUIwpqQoOoA==" forKey:@"__VIEWSTATE"];
+    [request setPostValue:@"/wEPDwUKMTU5Mzk3MTA0NA9kFgJmD2QWAgICDxYCHgVjbGFzcwUFbG9naW4WAgICD2QWBgIBDw8WAh4JTWF4TGVuZ3RoAglkZAIDDw8WAh4DS0VZBSFjdGwwMF9NYWluQ29udGVudF9jYXB0Y2hhNDMyMzE2NTFkZAIFDw8WBh4EVGV4dGUeCENzc0NsYXNzBQZzdWJtaXQeBF8hU0ICAmRkZIdAU40VRNIYqj2+sTHiu3Pfn4zLGc8LAChvKbzwUDZr" forKey:@"__VIEWSTATE"];
+
     [request setPostValue:account.username forKey:@"ctl00$MainContent$tbPhoneNumber"];
     [request setPostValue:account.password forKey:@"ctl00$MainContent$tbPassword"];
     [request setPostValue:@"Войти" forKey:@"ctl00$MainContent$btnEnter"];
