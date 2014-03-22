6d444620b0254e9bcf933b2a3268176009a22536
andrew <andrew.sinkevitch@gmail.com>
2014-03-22 21:57:19 +0300

    fix fo byfly

--- ByBalance/Classes/App/Balance/Loader/Loaders/BBLoaderByFly.m
+++ ByBalance/Classes/App/Balance/Loader/Loaders/BBLoaderByFly.m
@@ -18,44 +18,25 @@
 
 - (void) startLoader
 {
-    [self clearCookies:@"https://issa.beltelecom.by/cgi-bin/cgi.exe"];
+    [self clearCookies:@"https://issa.beltelecom.by/"];
     self.httpClient = [AFHTTPClient clientWithBaseURL:[NSURL URLWithString:@"https://issa.beltelecom.by/"]];
     [self setDefaultsForHttpClient];
     
     NSDictionary *params = [NSDictionary dictionaryWithObjectsAndKeys:
-                            self.account.username, @"mobnum",
-                            self.account.password, @"Password",
+                            self.account.username, @"oper_user",
+                            self.account.password, @"passwd",
+                            @"/main.html", @"redirect",
                             nil];
     //DDLogVerbose(@"%@", params);
   
-    [self.httpClient postPath:@"/cgi-bin/cgi.exe?function=is_login" parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
+    [self.httpClient postPath:@"main.html" parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
         
-        NSString *response1 = [[NSString alloc] initWithData:responseObject encoding:NSWindowsCP1251StringEncoding];
-        //NSString * response1 = operation.responseString;
-        //DDLogVerbose(@"Response1:\n%@", response1);
+        //NSString *response1 = [[NSString alloc] initWithData:responseObject encoding:NSWindowsCP1251StringEncoding];
+        NSString * response = operation.responseString;
+        //DDLogVerbose(@"Response:\n%@", response);
         
-        //cgi.exe?function=is_account
-        if ([response1 rangeOfString:@"cgi.exe?function=is_account"].location == NSNotFound)
-        {
-            [self doFinish];
-        }
-        else
-        {
-            [self.httpClient getPath:@"/cgi-bin/cgi.exe?function=is_account" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
-                
-                NSString *response2 = [[NSString alloc] initWithData:responseObject encoding:NSWindowsCP1251StringEncoding];
-                //NSString * response2 = operation.responseString;
-                //DDLogVerbose(@"Response2:\n%@", response2);
-                
-                [self extractInfoFromHtml:response2];
-                [self doFinish];
-                
-            } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-                
-                DDLogError(@"%@ step2 httpclient_error: %@", [self class], error.localizedDescription);
-                [self doFinish];
-            }];
-        }
+        [self extractInfoFromHtml:response];
+        [self doFinish];
         
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         
@@ -68,11 +49,16 @@
 {
     NSArray * arr = nil;
     BOOL extracted = NO;
-    
     //DDLogVerbose(@"%@", html);
     
+    if ([html rangeOfString:@"name=\"oper_user\""].location != NSNotFound)
+    {
+        self.loaderInfo.incorrectLogin = true;
+        return;
+    }
+    
     //balance
-    arr = [html stringsByExtractingGroupsUsingRegexPattern:@"Актуальный баланс:</td>\\s*<td class=light width=\"50%\">([^<]+)" caseInsensitive:YES treatAsOneLine:NO];
+    arr = [html stringsByExtractingGroupsUsingRegexPattern:@"Актуальный баланс:\\s*<b>\\s*([^<]+)" caseInsensitive:YES treatAsOneLine:NO];
     if (arr && [arr count] == 1)
     {
         self.loaderInfo.userBalance = [self decimalNumberFromString:[arr objectAtIndex:0]];
